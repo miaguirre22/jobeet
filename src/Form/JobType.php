0a21032f9f388a633a6ee8fc6d6c073081c16871
miaguirre22 <aguirremartinignacio@gmail.com>
2018-08-15 09:09:57 -0300

    formulario de jobs

---
+++ src/Form/JobType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Job;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+class JobType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type', TextType::class)
+            ->add('company', TextType::class)
+            ->add('logo', TextType::class)
+            ->add('url', UrlType::class)
+            ->add('position', TextType::class)
+            ->add('location', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('howToApply', TextType::class)
+            ->add('public', TextType::class)
+            ->add('activated', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('category', TextType::class)
+            ->add('token', TextType::class);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        'data_class' => Job::class,
+        ]);
+
+    }
+}
