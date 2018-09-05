b7a28084201a6a9e53c32ddec9d803d1ed5b85df
miaguirre22 <aguirremartinignacio@gmail.com>
2018-09-05 10:06:17 -0300

    dia_9: probando ejecutar Command

--- src/Command/CreateCategoryCommand.php
+++ src/Command/CreateCategoryCommand.php
@@ -27,6 +27,18 @@ class CreateCategoryCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'Category Creator',
+            '============',
+            '',
+        ]);
+
+        // outputs a message followed by a "\n"
+        $output->writeln('Whoa!');
+
+        // outputs a message without adding a "\n" at the end of the line
+        $output->write('You are about to ');
+        $output->write('create a category.');
     }
 }
