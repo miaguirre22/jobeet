ef425519e4a144b40f1c5f9b7ea0c91642dbb320
miaguirre22 <aguirremartinignacio@gmail.com>
2018-09-06 11:18:06 -0300

    dia_10: se creo y configuro el controlador CategoryController en la carpeta Admin

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * Lists all categories entities.
+     *
+     * @Route(
+     *		"/admin/categories", 
+     *		name="admin.category.list", 
+     *		methods="GET")
+     *
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function list(EntityManagerInterface $em) : Response
+    {
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        return $this->render('admin/category/list.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
